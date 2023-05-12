9738f45092ceed03578000c5958c70a817d1107c
Luis Niebla <lrios@repay.com>
2023-05-11 22:09:08 -0700

    Initial Commit

---
+++ SpotSummary/SpotSummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotSummaryApp.swift
+//  SpotSummary
+//
+//  Created by Luis Niebla Rios on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotSummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
