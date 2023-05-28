70958963c2545b3a19f9323b29ea672d1840b0c4
Luis Niebla <lrios@repay.com>
2023-05-27 17:13:24 -0700

    Start embedding example

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ python api/__init__.py
 
 
 ```
-curl https://localhost:5000/embeddings \
+curl http://localhost:5000/embeddings \
   -H "Content-Type: application/json" \
   -d '{
     "input": "Your text string goes here",
@@ -11,5 +11,32 @@ curl https://localhost:5000/embeddings \
   }'
 ```
 
+
+# What are embeddings
+- Text embeddings measure how related text strings are to each other. They are commonly used for recommendation, clustering, and classification.
+- Embeddings are simple vectors of floating point numbers, with distances between two points indicating their relatedness
+
+Price:
+- Model	Usage Ada v2	$0.0004 / 1K tokens
+
+The guide https://platform.openai.com/docs/guides/embeddings/use-cases has a bunch of good use cases. Notably, we will try following this guide for learning how to compare text
+
+https://github.com/openai/openai-cookbook/blob/main/text_comparison_examples.md
+
+
+
+# TODO
+
+## Setup embeddings for search
+1. Split text into chunks smaller than token limit
+2. Embed each chunk
+3. Store embeddings in vector engine
+
+- OpenAI recommends fine-tuning versus embedding for text classification. When we're going to do tags, maybe we look into that later
+
+
+## Dev stuff
 - docker-compose
 - db setup
+- Vercel deployment
+- Hook up front and back-end
--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,9 @@
+import openai
+import pandas as pd
 import requests
+import tiktoken
 from dotenv import dotenv_values, load_dotenv
+from openai.embeddings_utils import get_embeddings
 from quart import Quart, request
 
 app = Quart(__name__)
@@ -7,34 +11,55 @@ app.config.from_prefixed_env()
 print(load_dotenv())
 config = dotenv_values('.env')
 
+
+openai.api_key = config['OPENAI_SECRET']
+
+
 def run() -> None:
     app.run()
 
-@app.get("/health")
-async def health():
-    return {"status": "ok"}
+
+# embedding model parameters
+embedding_model = "text-embedding-ada-002"
+embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
+max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
+
 
 @app.post("/embeddings")
 async def embeddings():
-    json = await request.get_json()
-    print(json)
-    resp = requests.post(
-        'https://api.openai.com/v1/embeddings',
-        headers={
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {config["OPENAI_SECRET"]}'
-        },
-        json={
-            'input': 'Hello World',
-            'model': 'text-embedding-ada-002'
-        },
-    )
-    return resp.json()
+    input_datapath = 'api/data/Reviews.csv'
+    df = pd.read_csv(input_datapath, index_col=0)
+    df = df[['Time', 'ProductId', 'UserId', 'Score', 'Summary', 'Text']]
+    df = df.dropna()
+    df.head(2)
+
+    # subsample to 1k more recent reviews
+    top_n = 1000
+    df = df.sort_values(by='Time', ascending=False).tail(top_n * 2)
+    df.drop('Time', axis=1, inplace=True)
+
+    encoding = tiktoken.get_encoding(embedding_encoding)
+
+    # omit long reviews
+    df['n_tokens'] = df.Text.apply(lambda x: len(encoding.encode(x)))
+    df = df[df.n_tokens <= max_tokens].tail(top_n)
+
+    # get embeddings
+    df['embedding'] = df.Text.apply(lambda x: get_embeddings(x, embedding_model))
+    df.to_csv('data/reviews_embeddings.csv')
+    return str(len(df))
+    # return str(df)
 
 
 @app.post("/echo")
 async def echo():
     data = await request.get_json()
     return {"input": data, "extra": True}
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
 
 app.run()
